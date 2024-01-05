58fd6dcce2addca51fdf2da29c32d595df475f3f
backenbro <ukagha.nzubechukwu@yahoo.com>
2024-01-05 12:31:08 +0600

    Done with the course model

--- server/controller/CourseController.ts
+++ server/controller/CourseController.ts
@@ -5,6 +5,7 @@ import cloudinary from "cloudinary"
 import {
     createCourse
 } from "../services/CourseService"
+import CourseModel from "../models/CourseModel"
 
 export const uploadCourse = catchAsyncError (async (req:Request,res:Response,next:NextFunction) => {
     try {
@@ -31,3 +32,51 @@ export const uploadCourse = catchAsyncError (async (req:Request,res:Response,nex
     }
 })
 
+export const editCourse = catchAsyncError (async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const data = req.body 
+        const courseId = req.params.id 
+        const thumbnail = data.thumbnail 
+
+        if (thumbnail) {
+            await cloudinary.v2.uploader.destroy(thumbnail.public_id)
+
+            const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+                folder:"courses"
+            })
+
+            data.thumbnail = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url 
+            }
+        }
+
+        const course = await CourseModel.findByIdAndUpdate(courseId, data, {new:true})
+        res.status(201).json({
+            success:true,
+            course  
+        })
+
+    } catch (error) {
+        if (error instanceof Error) {
+            return next (new ErrorHandler (error.message, 500))
+        }
+    }
+})
+
+export const getSingleCourse = catchAsyncError (async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const courseId = req.params.id 
+        const course = await CourseModel.findById(courseId).
+        select(" -CourseData.videoUrl -CourseData.suggestion -CourseData.questions -CourseData.links")
+
+        res.status(200).json({
+            success:true,
+            course
+        })
+    } catch (error) {
+        if (error instanceof Error) {
+            return next ( new ErrorHandler (error.message, 500))
+        }
+    }
+})
--- server/routes/CourseRoute.ts
+++ server/routes/CourseRoute.ts
@@ -3,10 +3,14 @@ const CourseRouter = express.Router()
 
 import { isAuthenticated, isAuthorized } from "../ultis/auth"
 import { 
+    editCourse,
+    getSingleCourse,
     uploadCourse
  } from "../controller/CourseController"
 
 
 CourseRouter.post ('/create-course', isAuthenticated, isAuthorized ("admin"), uploadCourse)
+CourseRouter.put ('/edit-course/:id', isAuthenticated, isAuthorized ("admin"), editCourse)
+CourseRouter.get ('/get-course/:id', getSingleCourse)
 
 export default CourseRouter 
