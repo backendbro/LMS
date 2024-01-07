fa4a708f6acea6368c3654525093ea2fae06133e
backenbro <ukagha.nzubechukwu@yahoo.com>
2024-01-07 06:27:58 +0600

    Done with the model creatiion of notification and order

---
+++ server/models/NotificationModel.ts
@@ -0,0 +1,31 @@
+import mongoose, {Document, Schema, Model} from "mongoose"
+
+export interface INotification extends Document {
+    title:string,
+    message:string,
+    status:string,
+    userId:string 
+}
+
+const NotificationSchema = new Schema <INotification> ({
+    title:{
+        type:String,
+        required:true 
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true,
+        default:"unread"
+    },
+    userId:{
+        type:String,
+        required:true
+    }
+}, {timestamps:true}) 
+
+const NotificationModel:Model <INotification> = mongoose.model("Notification", NotificationSchema)
+export default NotificationModel 
